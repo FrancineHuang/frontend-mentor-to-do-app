ad8734b677807f364573168accc905925e7ee374
FrancineHuang <huangxin130@gmail.com>
2024-08-22 18:22:18 +0900

    add: add data and todo form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
+import TodoForm from './components/TodoForm.jsx';
 import BgDesktopLight from './images/bg-desktop-light.jpg'
 import BgDesktopDark from './images/bg-desktop-dark.jpg'
 
+const data = [
+  {id: 1, content: "Complete online JavaScript Course", completed: true},
+  {id: 2, content: "Jog around the park 3x", completed: false},
+  {id: 3, content: "10 minutes meditation", completed: false},
+  {id: 4, content: "Read for 1 hour", completed: false},
+  {id: 5, content: "Pick up groceries", completed: false},
+  {id: 6, content: "Complete Todo App from Frontendmentor", completed: false},
+]
+
 function App() {
   const [themeLight, setThemeLight] = useState(true)
+  const [todos, setTodos] = useState(data)
 
   const themeClass = themeLight? 'very-light-gray' : 'very-dark-blue'
   const bgImage = themeLight? `url(${BgDesktopLight})` : `url(${BgDesktopDark})`
 
   return (
     <div 
-      className={`${themeClass} bg-no-repeat bg-top bg-cover pt-80 pb-80`}
+      className={`wrapper ${themeClass} bg-no-repeat bg-top bg-cover pt-80 pb-80`}
       style={{ backgroundImage: bgImage }}
     >
       <div className='max-w-864 mx-auto'>
         <Header themeLight={themeLight} setThemeLight={setThemeLight}/>
+        <main>
+          <TodoForm
+            todos={todos}
+            setTodos={setTodos}
+            themeLight={themeLight}
+          />
+        </main>
       </div>
     </div>
   );
